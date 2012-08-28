23c8eb2ba0deb5ed97a7f1b9b7b623522e07b678
Tobias Rupp <tobias.rupp@gmx.com>
2012-08-28 22:11:37 +0300

    fix for articles country field failing migrations #2

--- db/migrate/20120826193722_add_related_country_to_articles.rb
+++
@@ -1,5 +0,0 @@
-class AddRelatedCountryToArticles < ActiveRecord::Migration
-  def change
-    add_column :articles, :related_country, :string
-  end
-end
--- db/migrate/20120826195034_change_country_of_article_translations.rb
+++
@@ -1,9 +0,0 @@
-class ChangeCountryOfArticleTranslations < ActiveRecord::Migration
-  def up
-  	rename_column :article_translations, :country, :ctry
-  end
-
-  def down
-  	rename_column :article_translations, :ctry, :country
-  end
-end
