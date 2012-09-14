cccdb1314b804ee8b5e89439317767207e359b05
Tobias Rupp <tobias.rupp@gmx.com>
2012-09-14 15:08:51 +0100

    Fix for post migration #4

--- db/migrate/20120914105829_add_fields_to_post_translations.rb
+++ db/migrate/20120914105829_add_fields_to_post_translations.rb
@@ -12,10 +12,10 @@ class AddFieldsToPostTranslations < ActiveRecord::Migration
 		Post.reset_column_information
 		I18n.locale = :de
 		Post.all.each do |post|
-			if post.text
+			if !post.text.blank?
 		  execute "UPDATE post_translations SET text=\"#{post.text.html_safe}\" WHERE post_ID='#{post.id}'"
 			end
-			if post.text_2
+			if !post.text_2.blank?
 		  execute "UPDATE post_translations SET text_2=\"#{post.text_2.html_safe}\" WHERE post_ID='#{post.id}'"
 			end
 		end
