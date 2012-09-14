6084abc0a26a4e4102a772d053b593584a9d2cba
Tobias Rupp <tobias.rupp@gmx.com>
2012-09-14 15:11:38 +0100

    Fix for post migration #5

--- db/migrate/20120914105829_add_fields_to_post_translations.rb
+++ db/migrate/20120914105829_add_fields_to_post_translations.rb
@@ -1,28 +1,28 @@
 class AddFieldsToPostTranslations < ActiveRecord::Migration
   
-  class Post < ActiveRecord::Base
-  end
+  # class Post < ActiveRecord::Base
+  # end
 
-  def self.up
+  def change
     add_column :post_translations, :text, :text
     add_column :post_translations, :text_2, :text
 
-    # migrate existing post data to translation table
-		say "Copying any existing data to translation table."
-		Post.reset_column_information
-		I18n.locale = :de
-		Post.all.each do |post|
-			if !post.text.blank?
-		  execute "UPDATE post_translations SET text=\"#{post.text.html_safe}\" WHERE post_ID='#{post.id}'"
-			end
-			if !post.text_2.blank?
-		  execute "UPDATE post_translations SET text_2=\"#{post.text_2.html_safe}\" WHERE post_ID='#{post.id}'"
-			end
-		end
+  #   # migrate existing post data to translation table
+		# say "Copying any existing data to translation table."
+		# Post.reset_column_information
+		# I18n.locale = :de
+		# Post.all.each do |post|
+		# 	if !post.text.blank?
+		#   execute "UPDATE post_translations SET text=\"#{post.text.html_safe}\" WHERE post_ID='#{post.id}'"
+		# 	end
+		# 	if !post.text_2.blank?
+		#   execute "UPDATE post_translations SET text_2=\"#{post.text_2.html_safe}\" WHERE post_ID='#{post.id}'"
+		# 	end
+		# end
   end
 
-  def self.down
-  	remove_column :post_translations, :text, :text
-    remove_column :post_translations, :text_2, :text
-  end
+  # def self.down
+  # 	remove_column :post_translations, :text, :text
+  #   remove_column :post_translations, :text_2, :text
+  # end
 end
