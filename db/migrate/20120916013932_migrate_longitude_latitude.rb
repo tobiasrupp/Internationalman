4b63eb1dcd9dc09bf749ee032757aa88c1d48e54
Tobias Rupp <tobias.rupp@gmx.com>
2012-09-16 03:40:55 +0100

    Added migration for lat/lon data (Radio) #4

--- db/migrate/20120916013932_migrate_longitude_latitude.rb
+++ db/migrate/20120916013932_migrate_longitude_latitude.rb
@@ -15,9 +15,10 @@ class MigrateLongitudeLatitude < ActiveRecord::Migration
   	count = 0
   	RadioTrack.all.each do |radio_track|
   		if !radio_track.longitude.blank? and !radio_track.latitude.blank?
+  			say "Row: #{radio_track.title}"
   			lon = radio_track.longitude.to_f
   			lat = radio_track.latitude.to_f
-  			radio_track.update_attributes!(:lon => lon, :lat => lat, :gmaps => true, :address => '')
+  			radio_track.update_attributes!(:lon => lon, :lat => lat, :gmaps => true, :address => 'Bensheim, Germany')
   			count = count + 1
   		end
   	end
