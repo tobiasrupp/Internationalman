740e8d34c31f1e6f37a8533f963a8a0ec679fb01
Tobias Rupp <tobias.rupp@gmx.com>
2012-10-16 22:43:26 +0100

    Added migration for Postgres Unaccent extension

--- db/migrate/20121004234454_add_unaccent_extension.rb
+++ db/migrate/20121004234454_add_unaccent_extension.rb
@@ -1,10 +1,10 @@
 class AddUnaccentExtension < ActiveRecord::Migration
-  # def up
-  #   execute "create extension unaccent"
-  # end
+  def up
+    execute "create extension unaccent"
+  end
 
-  # def down
-  #   execute "drop extension unaccent"
-  # end
+  def down
+    execute "drop extension unaccent"
+  end
 end
 
