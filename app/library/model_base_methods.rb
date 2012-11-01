87c086deaeda5df50420de6613c3b37a638c3823
Tobias Rupp <tobias.rupp@gmx.com>
2012-11-01 18:49:07 +0000

    Refactored model_base_methods.rb

--- app/library/model_base_methods.rb
+++ app/library/model_base_methods.rb
@@ -28,52 +28,40 @@ module ModelBaseMethods
   	# builds a search string with field values including German and English translations for full-text search
     original_locale = I18n.locale
     @separator = ', '
-    I18n.locale = :de
-    i = 0
-    # case self.class.name
-   	#  	when 'Video'
-    # 		search_string = 'TV, Video, Fernsehen'
-    #   when 'RadioTrack'
-    #     search_string = 'Radio, Audio, mp3'
-    #   when 'Post'
-    #     search_string = 'Blog, Blogeintrag, Posts'
-    #   when 'Article'
-    #     search_string = 'Stories, Artikel, Article'
-    # end
     search_string = get_general_keywords(self.class.name)
-    2.times do
-    # loop 2 times, first in German then in English
-      i += 1
-      if i == 2
-        I18n.locale = :en
-      end
-      search_string += check_and_add_value(self.title)
-	    search_string += check_and_add_value(self.short_title)
-	    search_string += check_and_add_value(self.url_title)
-	    search_string += check_and_add_value(self.author)
-      search_string += check_and_add_value(self.ctry)
-      search_string += check_and_add_value(self.address)
-      search_string += check_and_add_value(self.category_list)
-      if self.class.name == 'Video' or self.class.name == 'RadioTrack'
-        search_string += check_and_add_date(self.broadcast_date)
-  	    search_string += check_and_add_value(self.broadcaster)
-      elsif self.class.name == 'Post'
-        search_string += check_and_add_date(self.created_at)
-        search_string += check_and_add_value(self.text)
-        search_string += check_and_add_value(self.text_2)
-      elsif self.class.name == 'Article'
-        search_string += check_and_add_date(self.published_date)
-        search_string += check_and_add_value(self.published_in)
-        search_string += check_and_add_value(self.photos_by)
-        search_string += check_and_add_value(self.article_type)
-      end 
-  	end
+    I18n.locale = :de
+    search_string = build(search_string)
+    I18n.locale = :en
+    search_string = build(search_string)
   	I18n.locale = original_locale
 		return search_string
   end  
 
 private
-  
+
+  def build(search_string)
+    search_string += check_and_add_value(self.title)
+    search_string += check_and_add_value(self.short_title)
+    search_string += check_and_add_value(self.url_title)
+    search_string += check_and_add_value(self.author)
+    search_string += check_and_add_value(self.ctry)
+    search_string += check_and_add_value(self.address)
+    search_string += check_and_add_value(self.category_list)
+    if self.class.name == 'Video' or self.class.name == 'RadioTrack'
+      search_string += check_and_add_date(self.broadcast_date)
+      search_string += check_and_add_value(self.broadcaster)
+    elsif self.class.name == 'Post'
+      search_string += check_and_add_date(self.created_at)
+      search_string += check_and_add_value(self.text)
+      search_string += check_and_add_value(self.text_2)
+    elsif self.class.name == 'Article'
+      search_string += check_and_add_date(self.published_date)
+      search_string += check_and_add_value(self.published_in)
+      search_string += check_and_add_value(self.photos_by)
+      search_string += check_and_add_value(self.article_type)      
+    end 
+  end
+
   def check_and_add_value(value)
     if value.nil? or value.blank?
       return ''
