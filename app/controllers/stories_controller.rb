1c51771636f3e2eb269fa337e7f7048ec14399f2
Tobias Rupp <tobias.rupp@gmx.com>
2012-11-02 16:41:26 +0000

    Refactored Stories Controller

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -7,17 +7,13 @@ class StoriesController < ApplicationController
     current_article = Article.find(:first,
                             :include => :categories,
                             :order => "published_date DESC")
-    if current_article.nil?
-      flash.now[:notice] = "Keine Stories gefunden."
-      return
-    end
-
+    
+    return flash.now[:notice] = "Keine Stories gefunden." if current_article.nil?
+     
     # get stories of the same category
     current_category = current_article.categories.first
-    if current_category.nil?
-    	flash.now[:error] = "Keine Stories mit Kategorien gefunden."
-      return
-    end 
+    return flash.now[:error] = "Keine Stories mit Kategorien gefunden." if current_category.nil?
+
     redirect_to :action => :show_stories_by_category, :category => current_category.url_name, :article_title => current_article.url_title, :only_path => true
   end
 
@@ -31,19 +27,14 @@ class StoriesController < ApplicationController
                           :order => "display_section ASC, display_sequence ASC")
     
     @story_categories = get_categories_with_stories(categories)
-    if @story_categories.count == 0
-      flash.now[:notice] = "Keine Stories mit Kategorien gefunden."
-      return
-    end
+    return flash.now[:notice] = "Keine Stories mit Kategorien gefunden." if @story_categories.count == 0
 
   	# get all stories of requested category sorted by published_date
   	current_category = Category.find_by_url_name(params[:category])
 
-  	if current_category.nil?
-      # category unknown
-      flash.now[:error] = "Kategorie '#{params[:category]}' wurde nicht gefunden."
-      return
-    end  
+    # category unknown
+    return flash.now[:error] = "Kategorie '#{params[:category]}' wurde nicht gefunden." if current_category.nil?
+
     @stories = current_category.articles
     @selected_category = current_category
     if !params[:article_title]
@@ -53,11 +44,10 @@ class StoriesController < ApplicationController
       return
     end
     @selected_article = get_selected_article(@stories, params[:article_title])
-    if @selected_article.nil?
-      # requested story unknown
-      flash.now[:error] = "Story '#{params[:article_title]}' wurde nicht gefunden."
-      return
-    end
+    
+    # requested story unknown
+    return flash.now[:error] = "Story '#{params[:article_title]}' wurde nicht gefunden." if @selected_article.nil?
+
     @show_fb_like_button = true
     @content_section_column_width = get_content_column_width(@stories)
   	render :action => "show_stories"
@@ -71,8 +61,8 @@ private
       if category.articles.count > 0
         story_categories<<category
       end
-      return story_categories
     end
+    return story_categories
   end  
 
   def get_selected_article(articles, requested_article)
@@ -86,10 +76,7 @@ private
   end 
 
   def get_content_column_width(stories)
-    if long_titles?(stories) 
-      content_column_width = 4
-    else
-      content_column_width = 5
-    end
+      return 4 if long_titles?(stories) 
+      return 5
   end
 end
