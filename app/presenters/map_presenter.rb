60e8a7b6b550de80700670bf43c02a59f195a835
Tobias Rupp <tobias.rupp@gmx.com>
2012-11-03 18:29:35 +0000

    Refactored Map Presenter class

--- app/presenters/map_presenter.rb
+++ app/presenters/map_presenter.rb
@@ -21,26 +21,12 @@ private
 
   def build_articles_json
     articles_json = data[:articles].to_gmaps4rails do |article, marker|
-      is_corporate = false 
-      article.categories.each do |category|
-        if category.name == 'Corporate'
-          is_corporate = true
-          break
-        end
-      end
+      is_corporate = corporate_article?(article)
       marker.infowindow article_summary(article)
       if is_corporate == true
-        marker.title   article.title + ' (Corporate)'
-        marker.json({ :id => article.id, "picture" => "/assets/clothers_male_b_w.png",
-           "width" => 32,
-           "height" => 37,
-           "shadow_picture" => "http://maps.google.com/mapfiles/shadow50.png" ,
-           "shadow_width" => "37",
-           "shadow_height" => "34",
-           "shadow_anchor" => [10, 34]
-          })
+        marker = corporate_article_json(article, marker)
       else
-        marker.title   article.title + ' (Stories)'
+        marker.title  article.title + ' (Stories)'
         marker.json({ :id => article.id })
       end
     end
@@ -49,7 +35,7 @@ private
   def build_radio_tracks_json
     radio_tracks_json = data[:radio_tracks].to_gmaps4rails do |radio_track, marker|
       marker.infowindow radio_track_summary(radio_track)
-      marker.title   radio_track.title + ' (Radio)'
+      marker.title  radio_track.title + ' (Radio)'
       marker.json({ :id => radio_track.id })
     end
     return radio_tracks_json
@@ -57,7 +43,7 @@ private
   def build_videos_json
     videos_json = data[:videos].to_gmaps4rails do |video, marker|
       marker.infowindow video_summary(video)
-      marker.title   video.title + ' (TV)'
+      marker.title  video.title + ' (TV)'
       marker.json({ :id => video.id })
     end
     return videos_json
@@ -65,7 +51,7 @@ private
   def build_posts_json
     posts_json = data[:posts].to_gmaps4rails do |post, marker|
       marker.infowindow post_summary(post)
-      marker.title   post.title + ' (Blog)'
+      marker.title  post.title + ' (Blog)'
       marker.json({ :id => post.id })
     end
     return posts_json
@@ -82,5 +68,27 @@ private
   def post_summary(post)
     render :partial => 'posts/post_details', :locals => {:@selected_post => post, :@format_for_map => true}
   end
+  def corporate_article?(article)
+    is_corporate = false
+    article.categories.each do |category|
+      if category.name == 'Corporate'
+        is_corporate = true
+        break
+      end
+    end
+    return is_corporate
+  end
+  def corporate_article_json(article, marker)
+    marker.title  article.title + ' (Corporate)'
+    marker.json({ :id => article.id, "picture" => "/assets/clothers_male_b_w.png",
+       "width" => 32,
+       "height" => 37,
+       "shadow_picture" => "http://maps.google.com/mapfiles/shadow50.png" ,
+       "shadow_width" => "37",
+       "shadow_height" => "34",
+       "shadow_anchor" => [10, 34]
+      })
+    return marker
+  end  
 end
 
