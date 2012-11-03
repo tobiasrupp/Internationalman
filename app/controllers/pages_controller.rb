fb355700360b150b3278fa2d86974b4f2b3f99d4
Tobias Rupp <tobias.rupp@gmx.com>
2012-11-03 15:38:27 +0000

    Refactored Pages Controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -27,55 +27,54 @@ class PagesController < ApplicationController
     render(:layout => 'map')
   end
 
-
   def contact
-    # add_locale_to_url
-    
-  	if !@page = Page.find_by_page_type('CONTACT')
-      @page = Page.new
-  		@page.body = "<span class=\"alert alert-info\">Dies ist nur ein Beispieltext. Der richtige Text kann #{ view_context.link_to('hier', admin_pages_path )} gepflegt werden.</span>"
-  		@page.title = 'Beispieltitel'
-  	end
+    @page = get_static_page('CONTACT')
     render(:layout => 'pages')
   end
 
   def about
-  	if !@page = Page.find_by_page_type('ABOUT')
-      @page = Page.new
-  		@page.body = "<span class=\"alert alert-info\">Dies ist nur ein Beispieltext. Der richtige Text kann #{ view_context.link_to('hier', admin_pages_path ) } gepflegt werden.</span>"
-  		@page.title = 'Beispieltitel'
-  	end
+  	@page = get_static_page('ABOUT')
     render(:layout => 'pages')
   end
 
-  def map_without_javascript
-    # @gmap_string = 'http://maps.googleapis.com/maps/api/staticmap?sensor=false&size=140x100&maptype=hybrid'
+  private
 
-    # http://maps.googleapis.com/maps/api/staticmap?center=0,0&zoom=2&format=png&sensor=false&size=640x480&maptype=roadmap&style=invert_lightness:true|visibility:on
-    # add markers
-    # @articles = Article.all
-    # @articles.each do |article|
-    #   if !article.longitude.blank? and !article.latitude.blank?
-    #     @gmap_string = @gmap_string + '&markers=size:small|color:orange|' + article.longitude + ',' + article.latitude
-    #   end
-    # end
-    # @radio_tracks = RadioTrack.all
-    # @radio_tracks.each do |radio_track|
-    #   if !radio_track.longitude.blank? and !radio_track.latitude.blank?
-    #     @gmap_string = @gmap_string + '&markers=size:small|color:red|' + radio_track.longitude + ',' + radio_track.latitude
-    #   end
-    # end
-    # @videos = Video.all
-    # @videos.each do |video|
-    #   if !video.longitude.blank? and !video.latitude.blank?
-    #     @gmap_string = @gmap_string + '&markers=size:small|color:blue|' + video.longitude + ',' + video.latitude
-    #   end
-    # end
-    # @posts = Post.all
-    # @posts.each do |post|
-    #   if !post.longitude.blank? and !post.latitude.blank?
-    #     @gmap_string = @gmap_string + '&markers=size:small|color:green|' + post.longitude + ',' + post.latitude
-    #   end
-    # end
+  def get_static_page(page_type)
+    page = Page.where(:page_type => page_type).first
+    if page.nil?  
+      page = Page.new(:body => "<span class=\"alert alert-info\">Dies ist nur ein Beispieltext. Der richtige Text kann #{ view_context.link_to('hier', admin_pages_path )} gepflegt werden.</span>", :title => 'Beispieltitel')
+    end
+    return page
+  end
+
+  def map_without_javascript
+  # @gmap_string = 'http://maps.googleapis.com/maps/api/staticmap?sensor=false&size=140x100&maptype=hybrid'
+
+  # http://maps.googleapis.com/maps/api/staticmap?center=0,0&zoom=2&format=png&sensor=false&size=640x480&maptype=roadmap&style=invert_lightness:true|visibility:on
+  # add markers
+  # @articles = Article.all
+  # @articles.each do |article|
+  #   if !article.longitude.blank? and !article.latitude.blank?
+  #     @gmap_string = @gmap_string + '&markers=size:small|color:orange|' + article.longitude + ',' + article.latitude
+  #   end
+  # end
+  # @radio_tracks = RadioTrack.all
+  # @radio_tracks.each do |radio_track|
+  #   if !radio_track.longitude.blank? and !radio_track.latitude.blank?
+  #     @gmap_string = @gmap_string + '&markers=size:small|color:red|' + radio_track.longitude + ',' + radio_track.latitude
+  #   end
+  # end
+  # @videos = Video.all
+  # @videos.each do |video|
+  #   if !video.longitude.blank? and !video.latitude.blank?
+  #     @gmap_string = @gmap_string + '&markers=size:small|color:blue|' + video.longitude + ',' + video.latitude
+  #   end
+  # end
+  # @posts = Post.all
+  # @posts.each do |post|
+  #   if !post.longitude.blank? and !post.latitude.blank?
+  #     @gmap_string = @gmap_string + '&markers=size:small|color:green|' + post.longitude + ',' + post.latitude
+  #   end
+  # end
   end
 end
